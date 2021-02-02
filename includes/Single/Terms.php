dddb33e11dd367a23796527f7517afde59c5e93b
rvdforst <rolf.v.d.forst@fau.de>
2021-02-02 15:47:18 +0100

    Create Terms.php

---
+++ includes/Single/Terms.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace RRZE\Multilang\Single;
+
+defined('ABSPATH') || exit;
+
+use RRZE\Multilang\Functions;
+
+class Terms
+{
+    public function __construct()
+    {
+        add_filter('bloginfo', [$this, 'bloginfoFilter'], 10, 2);
+        add_filter('get_term', [$this, 'getTermFilter'], 10, 2);
+        add_action('load-edit-tags.php', [__CLASS__, 'removeGetTermFilter'], 10, 0);
+    }
+
+    public function bloginfoFilter($output, $show)
+    {
+        if (!Translation::is_ready()) {
+            return $output;
+        }
+
+        if ('name' == $show) {
+            $output = Functions::translate('blogname', 'blogname', $output);
+        } elseif ('description' == $show) {
+            $output = Functions::translate('blogdescription', 'blogdescription', $output);
+        }
+
+        return $output;
+    }
+
+    public function getTermFilter($term, $taxonomy)
+    {
+        if (!Translation::is_ready()) {
+            return $term;
+        }
+
+        if ($term instanceof \WP_Term) {
+            $term = Functions::translateTerm($term);
+        }
+
+        return $term;
+    }
+
+    public static function removeGetTermFilter()
+    {
+        remove_filter('get_term', [__CLASS__, 'getTermFilter']);
+    }
+}
