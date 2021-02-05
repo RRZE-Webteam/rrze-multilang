c699912243359d78ae7ae3dc1e43d727bc13bad1
rvdforst <rolf.v.d.forst@fau.de>
2021-02-05 15:58:53 +0100

    Update rrze-multilang.php

--- rrze-multilang.php
+++ rrze-multilang.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE Multilang
 Plugin URI:      https://github.com/RRZE-Webteam/rrze-multilang
 Description:     Multilanguage plugin for WordPress.
-Version:         0.1.7
+Version:         0.1.8
 Author:          RRZE-Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v2
