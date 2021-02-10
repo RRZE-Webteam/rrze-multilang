b6a333a3400c2365f9285740ca9c2b8945939e88
rvdforst <rolf.v.d.forst@fau.de>
2021-02-10 18:07:40 +0100

    Create index.js

---
+++ assets/block-editor/src/multiple/index.js
@@ -0,0 +1,32 @@
+import { registerPlugin } from '@wordpress/plugins';
+import { addQueryArgs, hasQueryArg } from '@wordpress/url';
+import apiFetch from '@wordpress/api-fetch';
+
+import render from './render';
+
+registerPlugin('rrze-multilang-language-panel', {
+    render,
+    icon: 'translation'
+});
+
+apiFetch.use((options, next) => {
+    const postLang = rrzeMultilang.currentPost.lang;
+
+    if (postLang) {
+        if (
+            typeof options.url === 'string' &&
+            !hasQueryArg(options.url, 'lang')
+        ) {
+            options.url = addQueryArgs(options.url, { lang: postLang });
+        }
+
+        if (
+            typeof options.path === 'string' &&
+            !hasQueryArg(options.path, 'lang')
+        ) {
+            options.path = addQueryArgs(options.path, { lang: postLang });
+        }
+    }
+
+    return next(options, next);
+});
