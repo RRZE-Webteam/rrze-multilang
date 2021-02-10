4ac73cb16f64b206b421715310fb2c472c26743d
rvdforst <rolf.v.d.forst@fau.de>
2021-02-10 18:07:24 +0100

    Create index.asset.php

---
+++ assets/block-editor/build/multiple/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => '205f66dad79994a6488a45a40a8e8c01');
