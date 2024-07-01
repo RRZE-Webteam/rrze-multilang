ce80a8027c0f341bc9965553059bb73e7cc1f974
rvdforst <rolf.v.d.forst@fau.de>
2024-07-01 17:50:33 +0200

    Create block-editor-single.asset.php

---
+++ build/block-editor-single.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '5c7a3c3883278b325796');
