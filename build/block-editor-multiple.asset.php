19d74acb65f7590f0421f11b8565ca39102e63c5
rvdforst <rolf.v.d.forst@fau.de>
2024-07-01 17:50:28 +0200

    Create block-editor-multiple.asset.php

---
+++ build/block-editor-multiple.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'f2794f1f53eda88bf213');
