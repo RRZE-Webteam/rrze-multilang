b337f9efaa8f88b5fb2828b0871244d0c41466f8
rvdforst <rolf.v.d.forst@fau.de>
2024-07-01 17:50:13 +0200

    Delete index.js

--- assets/block-editor/src/multiple/index.js
+++
@@ -1,8 +0,0 @@
-import { registerPlugin } from '@wordpress/plugins';
-
-import render from './render';
-
-registerPlugin('rrze-multilang-language-panel', {
-    render,
-    icon: 'translation'
-});
