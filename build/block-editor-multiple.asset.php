77b1db2b278af99194f68e29b56808316daf1ef3
Rolf <rolf.v.d.forst@fau.de>
2024-07-12 14:09:08 +0200

    Update build files

--- build/block-editor-multiple.asset.php
+++ build/block-editor-multiple.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'ae20eedffbfada2e442f');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '50d965e46a61b21dcb94');
