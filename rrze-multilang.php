f65393cfd2802e714f21812f59d4ed69eca12900
Rolf <rolf.v.d.forst@fau.de>
2024-11-22 12:39:19 +0100

    Update rrze-multilang.php

--- rrze-multilang.php
+++ rrze-multilang.php
@@ -3,25 +3,21 @@
 /*
 Plugin Name:        RRZE Multilang
 Plugin URI:         https://github.com/RRZE-Webteam/rrze-multilang
+Version:            1.2.5
 Description:        Multilanguage plugin for WordPress.
-Version:            1.2.4
-Requires at least:  6.6
-Requires PHP:       8.2
-Author:             RRZE-Webteam
+Author:             RRZE Webteam
 Author URI:         https://blogs.fau.de/webworking/
-License:            GNU General Public License v3
+License:            GNU General Public License Version 3
 License URI:        https://www.gnu.org/licenses/gpl-3.0.html
 Text Domain:        rrze-multilang
 Domain Path:        /languages
-Update URI:         https://github.com/RRZE-Webteam/rrze-multilang
+Requires at least:  6.7
+Requires PHP:       8.2
 */
 
 namespace RRZE\Multilang;
 
 defined('ABSPATH') || exit;
-
-const RRZE_PHP_VERSION = '8.2';
-const RRZE_WP_VERSION = '6.6';
 
 /**
  * SPL Autoloader (PSR-4).
@@ -45,49 +41,28 @@ spl_autoload_register(function ($class) {
     }
 });
 
+// Register activation hook for the plugin
 register_activation_hook(__FILE__, __NAMESPACE__ . '\activation');
+
+// Register deactivation hook for the plugin
 register_deactivation_hook(__FILE__, __NAMESPACE__ . '\deactivation');
+
+/**
+ * Add an action hook for the 'plugins_loaded' hook.
+ *
+ * This code hooks into the 'plugins_loaded' action hook to execute a callback function when
+ * WordPress has fully loaded all active plugins and the theme's functions.php file.
+ */
 add_action('plugins_loaded', __NAMESPACE__ . '\loaded', 9);
 
-/**
- * Loads a plugin’s translated strings.
- */
-function loadTextdomain()
-{
-    load_plugin_textdomain('rrze-multilang', false, sprintf('%s/languages/', dirname(plugin_basename(__FILE__))));
-}
-
-/**
- * System requirements verification.
- * @return string Return an error message.
- */
-function systemRequirements(): string
-{
-    $error = '';
-    if (version_compare(PHP_VERSION, RRZE_PHP_VERSION, '<')) {
-        $error = sprintf(
-            /* translators: 1: Server PHP version number, 2: Required PHP version number. */
-            __('The server is running PHP version %1$s. The Plugin requires at least PHP version %2$s.', 'rrze-multilang'),
-            PHP_VERSION,
-            RRZE_PHP_VERSION
-        );
-    } elseif (version_compare($GLOBALS['wp_version'], RRZE_WP_VERSION, '<')) {
-        $error = sprintf(
-            /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
-            __('The server is running WordPress version %1$s. The Plugin requires at least WordPress version %2$s.', 'rrze-multilang'),
-            $GLOBALS['wp_version'],
-            RRZE_WP_VERSION
-        );
-    }
-    return $error;
-}
+// Load the plugin's text domain for localization.
+add_action('init', fn() => load_plugin_textdomain('rrze-multilang', false, dirname(plugin_basename(__FILE__)) . '/languages'));
 
 /**
  * Activation callback function.
  */
 function activation()
 {
-    loadTextdomain();
     if ($error = systemRequirements()) {
         deactivate_plugins(plugin_basename(__FILE__));
         wp_die(
@@ -112,46 +87,109 @@ function deactivation()
 }
 
 /**
- * Instantiate Plugin class.
- * @return object Plugin
+ * Singleton pattern for initializing and accessing the main plugin instance.
+ *
+ * This method ensures that only one instance of the Plugin class is created and returned.
+ *
+ * @return Plugin The main instance of the Plugin class.
  */
 function plugin()
 {
+    // Declare a static variable to hold the instance.
     static $instance;
+
+    // Check if the instance is not already created.
     if (null === $instance) {
+        // Add a new instance of the Plugin class, passing the current file (__FILE__) as a parameter.
         $instance = new Plugin(__FILE__);
     }
+
+    // Return the main instance of the Plugin class.
     return $instance;
 }
 
 /**
- * Execute on 'plugins_loaded' API/action.
- * @return void
+ * Check system requirements for the plugin.
+ *
+ * This method checks if the server environment meets the minimum WordPress and PHP version requirements
+ * for the plugin to function properly.
+ *
+ * @return string An error message string if requirements are not met, or an empty string if requirements are satisfied.
+ */
+function systemRequirements(): string
+{
+    // Get the global WordPress version.
+    global $wp_version;
+
+    // Get the PHP version.
+    $phpVersion = phpversion();
+
+    // Initialize an error message string.
+    $error = '';
+
+    // Check if the WordPress version is compatible with the plugin's requirement.
+    if (!is_wp_version_compatible(plugin()->getRequiresWP())) {
+        $error = sprintf(
+            /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
+            __('The server is running WordPress version %1$s. The plugin requires at least WordPress version %2$s.', 'rrze-multilang'),
+            $wp_version,
+            plugin()->getRequiresWP()
+        );
+    } elseif (!is_php_version_compatible(plugin()->getRequiresPHP())) {
+        // Check if the PHP version is compatible with the plugin's requirement.
+        $error = sprintf(
+            /* translators: 1: Server PHP version number, 2: Required PHP version number. */
+            __('The server is running PHP version %1$s. The plugin requires at least PHP version %2$s.', 'rrze-multilang'),
+            $phpVersion,
+            plugin()->getRequiresPHP()
+        );
+    }
+
+    // Return the error message string, which will be empty if requirements are satisfied.
+    return $error;
+}
+
+/**
+ * Handle the loading of the plugin.
+ *
+ * This function is responsible for initializing the plugin, loading text domains for localization,
+ * checking system requirements, and displaying error notices if necessary.
  */
 function loaded()
 {
-    loadTextdomain();
+    // Trigger the 'loaded' method of the main plugin instance.
     plugin()->loaded();
+
+    // Check system requirements and store any error messages.
     if ($error = systemRequirements()) {
+        // If there is an error, add an action to display an admin notice with the error message.
         add_action('admin_init', function () use ($error) {
+            // Check if the current user has the capability to activate plugins.
             if (current_user_can('activate_plugins')) {
-                $pluginData = get_plugin_data(plugin()->getFile());
-                $pluginName = $pluginData['Name'];
+                // Get plugin data to retrieve the plugin's name.
+                $pluginName = plugin()->getName();
+
+                // Determine the admin notice tag based on network-wide activation.
                 $tag = is_plugin_active_for_network(plugin()->getBaseName()) ? 'network_admin_notices' : 'admin_notices';
+
+                // Add an action to display the admin notice.
                 add_action($tag, function () use ($pluginName, $error) {
                     printf(
                         '<div class="notice notice-error"><p>' .
                             /* translators: 1: The plugin name, 2: The error string. */
-                            __('Plugins: %1$s: %2$s', 'rrze-multilang') .
+                            esc_html__('Plugins: %1$s: %2$s', 'rrze-multilang') .
                             '</p></div>',
-                        esc_html($pluginName),
-                        esc_html($error)
+                        $pluginName,
+                        $error
                     );
                 });
             }
         });
+
+        // Return to prevent further initialization if there is an error.
         return;
     }
-    $main = new Main;
-    $main->onLoaded();
+
+    // If there are no errors, create an instance of the 'Main' class and trigger its 'loaded' method.
+    (new Main)->loaded();
 }
